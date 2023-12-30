829d396cb843cc33a8734a217f2abc9f433357a7
Alexandra Opritoiu <alexandra.opritoiu@gmail.com>
2023-12-30 15:26:35 +0200

    Task 3 done

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,13 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+const gallery = document.querySelector('.gallery');
+const galleryHTML = images.map(image =>
+  `<li class="gallery-item">
+    <img src = "${image.url}" alt="${image.alt}"></img>
+  </li>`);
+
+ gallery.insertAdjacentHTML('beforeend', galleryHTML);
+
