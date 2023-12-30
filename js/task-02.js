338ad7f1b8622d1ac163664e780ab08cbb44e74b
Alexandra Opritoiu <alexandra.opritoiu@gmail.com>
2023-12-30 15:11:25 +0200

    Task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+
+ingredients.forEach(ingredient => {
+  const elementFromList = document.createElement('li');
+  elementFromList.textContent = ingredient;
+  elementFromList.classList.add('item');
+  ingredientsList.appendChild(elementFromList);
+});
+
+
+
+
