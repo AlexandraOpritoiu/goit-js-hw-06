28694e4e45a897a7a16d7ec8b29ebf5a2c6f8823
Alexandra Opritoiu <alexandra.opritoiu@gmail.com>
2024-01-01 09:37:57 +0200

    Task 4 done

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+let counterValue = 0;
+const counterElement = document.getElementById('counter');
+const valueElement = document.getElementById('value');
+
+counterElement.addEventListener('click', (event) => {
+    const action = event.target.dataset.action;
+
+    if (action === 'decrement') {
+        counterValue--;
+    }
+    else if (action === 'increment') {
+        counterValue++;
+    }
+    valueElement.textContent = counterValue;
+}
+);
+
+
