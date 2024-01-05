aaf51e44945e1bf9f1330d21a5ed83dad5ffdbf9
Alexandra Opritoiu <alexandra.opritoiu@gmail.com>
2024-01-05 19:20:16 +0200

    Task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyColor = document.body;
+const spanColor = document.querySelector('.color');
+const changedColor = document.querySelector('.change-color');
+
+changedColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  bodyColor.style.backgroundColor = randomColor;
+});
